f9270d017f5768c1927ff9bdbb4c5582783e6f7a
Zac <zombiezac@att.net>
2019-01-28 13:19:38 -0700

    test for vectors and arrays

---
+++ TestProject/Controller/Runner.cpp
@@ -0,0 +1,17 @@
+//
+//  Runner.cpp
+//  TestProject
+//
+//  Created by Durham, Zachary on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+    Controller app;
+    app.start();
+    
+    return 0;
+}
